25a75ecad3019f3b7b78f69e68ba43b6acefcdb3
De'Andre Brown <browndeandre08@yahoo.com>
2023-05-26 14:53:04 -0400

    added router links

--- src/App.vue
+++ src/App.vue
@@ -2,14 +2,15 @@
 
 </script>
 
+
 <template>
 <v-layout>
-  <v-app-bar>
 
-<v-app-bar-title>Brief</v-app-bar-title>
-
-  </v-app-bar>
-
+  <nav>
+  <router-link to="/">Home</router-link> |
+  <router-link to="/about">About</router-link>
+</nav>
+<router-view></router-view>
 </v-layout>
 </template>
 
